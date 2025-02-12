bef2a722a96508fd53e173dbdb1d5916acda9adb
Franz Marlo Andal <andalfranzb@gmail.com>
2025-02-12 09:08:17 +0800

    Debug False

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zj+qnul1qqq(y7bh41p#&_6h8v++&)&0ckmw#-&nga_-+&-ow$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
