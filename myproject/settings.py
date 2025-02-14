589715a5fc235ef42d765bfe72b777be90b1a03d
Franz Marlo <andalfranzb@gmail.com>
2025-02-14 15:02:28 +0800

    db

--- myproject/settings.py
+++ myproject/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'leonaidas',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'postgres',
-        'PORT': '5432',       
+        'NAME': os.getenv('DB_NAME', 'leonaidas'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
