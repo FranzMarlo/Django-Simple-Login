285afc24feeab8d46681f1f785cf80b5c70cea15
Franz Marlo <andalfranzb@gmail.com>
2025-02-14 14:29:37 +0800

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zj+qnul1qqq(y7bh41p#&_6h8v++&)&0ckmw#-&nga_-+&-ow$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
 
 
 # Application definition
